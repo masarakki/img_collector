f7b7ed46c85ec4a14b7327ad3a62aa857b60fee9
Yamada Masaki <masaki@hisme.net>
2011-10-02 05:53:18 +0900

    first init

---
+++ Gemfile
@@ -0,0 +1,33 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+
+group :production do
+  gem 'mysql2'
+end
+
+group :test, :development do
+  gem "sqlite3"
+  gem "rspec-rails"
+  gem "auto_mock"
+  gem "webrat"
+  gem "autotest"
+  gem "cover_me"
+end
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+gem 'capistrano'
+
+group :test do
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageCollector::Application.config.secret_token = '0ccb06f5a0f74cb65be8090365a0e9eb11915746fe94f31cefb57aab815953bd07b06dc690f55d9dd5ff9c64f4cbf412f522fb4c0d270c1bf4d78acd472d73da'
